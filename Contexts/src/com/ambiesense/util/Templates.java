65d1b3445253eda2e92cc5f49cf5392a5b3669e5
hansmy <hans@ambiesense.com>
2012-10-30 00:48:46 +0000

    The GPL License version 3 has now been populated in all software parts, and read me file has been added.
    Signed-off-by: hansmy <hans@ambiesense.com>

--- Contexts/src/com/ambiesense/util/Templates.java
+++ Contexts/src/com/ambiesense/util/Templates.java
@@ -1,23 +1,42 @@
-/*-----------------------------------------------------------------------
+/*=================================================================
+AmbieSense Context Software GPL Source Code.
 
-  AmbieSense Ltd  
+Copyright (C) 2005 and onwards by AmbieSense Limited, 
+ and/or its subsidiaries and co-founders. AmbieSense Limited
+ is an R&D-focused company incorporated in Scotland.
 
-  -----------------------------------------------------------------------
+This file is part of the AmbieSense Context Software GPL Source Code.
 
- (C) Copyright from 2005 and onwards by AmbieSense Ltd.	All rights reserved.
+The AmbieSense Context Software GPL Source Code is free software: 
+you can redistribute it and/or modify it under the terms of the 
+GNU General Public License Version 3 as published by
+the Free Software Foundation.
 
- NOTICE:  All information contained herein or attendant hereto is,
-          and remains, the property of AmbieSense Ltd.  Many of the
-          intellectual and technical concepts contained herein are
-          proprietary to AmbieSense Ltd and may be covered
-          by National and Foreign Patents or Patents Pending, or are
-          protected as trade secrets.  Any dissemination of this
-          information or reproduction of this material is strictly
-          forbidden unless prior written permission is obtained
-          from AmbieSense Ltd.
+The AmbieSense Context Software Source Code is distributed in 
+the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or 
+FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
 
- -----------------------------------------------------------------------
-*/
+You should have received a copy of the GNU General Public License
+along with The AmbieSense Context Software Source Code. 
+If not, see <http://www.gnu.org/licenses/>.
+
+In addition, the The AmbieSense Context Software Source Code is also 
+subject to certain additional terms. You should have received a copy 
+of these additional terms immediately following the terms and 
+conditions of the GNU General Public License which 
+accompanied the The AmbieSense Context Software Source Code. 
+If not, please request a copy in writing from id Software at the address below.
+
+If you have questions concerning this license or the applicable 
+additional terms, you may contact in writing: 
+AmbieSense Limited, 7 Queens Gardens, Aberdeen, 
+AB25 4YD, Scotland; or by email: 
+kontactATambiesense.com (replace "AT" with "@").
+============================================================ */
+
+
 
 package com.ambiesense.util;
 
